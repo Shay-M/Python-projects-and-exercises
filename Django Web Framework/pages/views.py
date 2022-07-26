8b9021bec309a1d1f056eb93cca611ab5d3035da
“ShayM” <shay1mualem@gmail.com>
2022-07-26 18:01:46 +0300

    .

---
+++ Django Web Framework/pages/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+# args and **kwargs are mostly used in function definitions.
+#  args and **kwargs allow you to pass an unspecified number of arguments to
+# a function, so when writing the function definition, you do not need to know
+# how many arguments will be passed to your function. *args is used to send a
+# non-keyworded variable length argument list to the function. Here’s an example
+#  to help you get a clear
+
+# https://book.pythontips.com/en/latest/args_and_kwargs.html
+
+
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request)
+    # return HttpResponse("<h1>hii</h1>")
+    return render(request, "home.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse("<h1>hii</h1>")
--- Django Web Framework/products/models.py
+++ Django Web Framework/products/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=120)  # ! max_length = required
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)  # blank is required
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(blank=False, null=False)
-    featured = models.BooleanField(default=False)  # null=True, default=True
+    # if we add new Field : null=True, default=True
+    featured = models.BooleanField(default=False)
