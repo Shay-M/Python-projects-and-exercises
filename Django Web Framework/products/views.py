95a96d98747a63fda829368fba671b934ceaa4ec
“ShayM” <shay1mualem@gmail.com>
2022-07-27 18:12:17 +0300

    update

--- Django Web Framework/products/views.py
+++ Django Web Framework/products/views.py
@@ -8,7 +8,9 @@ from .models import Product
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
 
-    context = {'title': obj.title,
-               'description': obj.description}
+    # context = {'title': obj.title,
+    #            'description': obj.description}
 
-    return render(request, "product/detail.html", context)
+    context = {'object': obj}
+
+    return render(request, "products/product_detail.html", context)
